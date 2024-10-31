02e2f9926d213ce0c2f1207a0043a4777e91824a
Julio Cesar Baptistella <juliocbaptistella@gmail.com>
2024-10-31 17:57:43 -0300

    add

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ import 'package:news/pages/login_page.dart';
 import 'package:news/pages/profile_account_page.dart';
 import 'package:news/pages/splash_screen_page.dart';
 
+const FirebaseOptions firebaseOptions = FirebaseOptions(
+  apiKey: "AIzaSyB26blUxztettoxshVTkopupptvoe9_vlU",
+  authDomain: "news-b8d1c.firebaseapp.com",
+  projectId: "news-b8d1c",
+  storageBucket: "news-b8d1c.firebasestorage.app",
+  messagingSenderId: "5143857423",
+  appId: "1:5143857423:web:fd5e3112047bade394167a",
+  measurementId: "G-PMM1606LPD"
+);
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: firebaseOptions); // Inicializa o Firebase com as opções
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
